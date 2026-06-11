39efd1b484ad3c7345eb156e2f4cad8d73c7d3d8
IgorKulduaev <and.sobol39@mail.ru>
2026-06-11 10:45:09 +0200

    Add files via upload

---
+++ Lab_11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Student
+{
+    public string Surname { get; set; }
+    public List<int> Grades { get; set; }
+    public int TotalScore => Grades.Sum();
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Student> classStudents = new List<Student>
+        {
+            new Student { Surname = "Иванов", Grades = new List<int> { 5, 4, 5, 5 } },
+            new Student { Surname = "Смирнов", Grades = new List<int> { 5, 5, 5, 5 } },
+            new Student { Surname = "Кузнецов", Grades = new List<int> { 3, 4, 4, 3 } },
+        };
+
+        Student topStudent = classStudents
+            .OrderByDescending(s => s.TotalScore)
+            .FirstOrDefault();
+
+        if (topStudent != null)
+        {
+            Console.WriteLine($"Ученик с максимальной суммой оценок: {topStudent.Surname} (Сумма: {topStudent.TotalScore})");
+        }
+        else
+        {
+            Console.WriteLine("Список учеников пуст.");
+        }
+    }
+}
